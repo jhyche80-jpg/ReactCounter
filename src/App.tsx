62a7dc83d839198ee720918043af68eae90db6cb
Javon <jhyche80@gmail.com>
2025-12-15 14:27:04 -0500

    I made more style choices on the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,95 @@
 import './App.css'
 import CounterControls from './components/CounterControls/CounterControls'
 import CounterDisplay from './components/CounterDisplay/CounterDisplay'
-import React, {  useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Count, Status, Step } from './components/types'
 import StepInput from './components/StepCount/StepInput'
 import CounterHistory from './components/History/CounterHistory'
 function App() {
-  const [count,setCount]= useState<Count>(0)
+  const [count, setCount] = useState<Count>(0)
   const [stepp, setStepp] = useState<Step>(1)
-  const [history,setHistory]= useState<Count[]>([])
-  const [message,setMessage]= useState<Status>("idle")
-  
+  const [history, setHistory] = useState<Count[]>([])
+  const [message, setMessage] = useState<Status>("idle")
 
 
-  useEffect(()=>{
-    const saveCount =  ()=>{
-     setMessage("Saving...")
 
-     setTimeout(() => {
-      setMessage("Changes saved!")
-      setTimeout(() => setMessage("idle"), 2000);
-     }, 1000);
-     
+  useEffect(() => {
+    const saveCount = () => {
+      setMessage("Saving...")
+
+      setTimeout(() => {
+        setMessage("Changes saved!")
+        setTimeout(() => setMessage("idle"), 2000);
+      }, 1000);
+
     }
     saveCount()
 
-  },[count])
+  }, [count])
 
-useEffect(()=>{
-  const handleKeydown=(e:KeyboardEvent)=>{
-    if (e.key==="ArrowUp"){
+  useEffect(() => {
+    const handleKeydown = (e: KeyboardEvent) => {
+      if (e.key === "ArrowUp") {
 
-      e.preventDefault()
-     setCount(prev=> prev +stepp)
-     setHistory(prev=>[...prev,count])
-     
+        e.preventDefault()
+        setCount(prev => prev + stepp)
+        setHistory(prev => [...prev, count])
+
+      }
+      if (e.key === "ArrowDown") {
+        e.preventDefault()
+        setCount(prev => prev - stepp)
+        setHistory(prev => [...prev, count])
+
+
+      }
+
     }
-    if(e.key === "ArrowDown") {
-      e.preventDefault()
-      setCount(prev=> prev-stepp)
-    setHistory(prev=>[...prev,count])
-
-
+    document.addEventListener("keydown", handleKeydown)
+    return () => {
+      document.removeEventListener("keydown", handleKeydown)
     }
-      
-}
-document.addEventListener("keydown", handleKeydown)
-return ()=> {
-  document.removeEventListener("keydown", handleKeydown)
-}
 
-},[stepp,count])
+  }, [stepp, count])
 
-  const handleIncrement = () =>{
-  setCount(prevCount =>prevCount + stepp)
-  setHistory(prevHistory=>[...prevHistory,count])
-  
-  
- }
- const handleDecrement = ()=>{
-  setCount(prevCount => prevCount-stepp)
-  setHistory(prev=>[...prev,count])
-  if(count===0){
-    setCount(0)
+  const handleIncrement = () => {
+    setCount(prevCount => prevCount + stepp)
+    setHistory(prevHistory => [...prevHistory, count])
+
+
   }
- }
- const handleReset = ()=> {
-  setCount(0)
-  setHistory([])
-  localStorage.clear()
-  
- }
- const handleCHangeStep= (e:React.ChangeEvent<HTMLInputElement>)=> {
-  setStepp(parseInt(e.target.value))
- }
+  const handleDecrement = () => {
+    setCount(prevCount => prevCount - stepp)
+    setHistory(prev => [...prev, count])
+    if (count === 0) {
+      setCount(0)
+    }
+  }
+  const handleReset = () => {
+    setCount(0)
+    setHistory([])
+    localStorage.clear()
+
+  }
+  const handleCHangeStep = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStepp(parseInt(e.target.value))
+  }
   return (
     <>
-    <h2>Current Count:</h2>
-    <CounterDisplay count={count}/>
-    <StepInput step={stepp} onChangeStep={handleCHangeStep}/>
-    <CounterControls 
-    onIncrement={handleIncrement}
-    onDecrement={handleDecrement}
-    onReset={handleReset}/>
-    <p>{message}</p>
-    <CounterHistory history={history} />
+      <div id='container'>
+
+        <h2>Current Count:</h2>
+        <CounterDisplay count={count} />
+        <StepInput step={stepp} onChangeStep={handleCHangeStep} />
+        <CounterControls
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+          onReset={handleReset} />
+        <p>{message}</p>
+        <CounterHistory history={history} />
+        <p>Press the Up and down arrows to control the count</p>
+      </div>
+
     </>
 
   )
